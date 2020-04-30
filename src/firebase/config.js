dc468eba00fcd69730528bae6ee8f10982b05ebe
reinatrychadani <trycha67@gmail.com>
2020-04-30 12:06:11 +0700

    Menyelesaikan praktikum pada Modul-12 Firebase realtime

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdtWHsGNxFeYWdie_f-BYvZZwyYWPutQ0",
+    authDomain: "frameworkfirebaselogin-802a1.firebaseapp.com",
+    databaseURL: "https://frameworkfirebaselogin-802a1.firebaseio.com",
+    projectId: "frameworkfirebaselogin-802a1",
+    storageBucket: "frameworkfirebaselogin-802a1.appspot.com",
+    messagingSenderId: "520571857748",
+    appId: "1:520571857748:web:30e3f1b4a1d116ef1bb6c1",
+    measurementId: "G-Z3Q7DD01EK"
+  };
+
+  export default firebaseConfig;
